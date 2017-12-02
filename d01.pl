b8d1dffbaf0dfabeee6b8278107252bbb96dfb63
Gustaf Erikson <gerikson@gmail.com>
2017-12-02 07:53:48 +0000

    Formatting

--- d01.pl
+++ d01.pl
@@ -7,16 +7,19 @@
 use 5.016;    # implies strict, provides 'say'
 use warnings;
 use autodie;
+use List::Util qw/sum/;
+use Data::Dumper;
 
 #### INIT - load input data into array
 my $testing = 0;
 my @input;
 my $file = $testing ? 'test.txt' : 'input.txt';
+{
     open( my $fh, '<', "$file" );
     while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
+}
 
 ### CODE
-# there's just one line in this problem
 my @list   = split( //, shift @input );
 my $length = scalar @list;
 my $sum_1  = 0;
@@ -25,10 +28,9 @@ my $sum_2  = 0;
 # circular list, just make a copy to avoid funky rollover arithmetic
 my @check = ( @list, @list );
 for ( my $i = 0 ; $i <= $length - 1 ; $i++ ) {
-    # first part
-    if ( $check[$i] == $check[ $i + 1 ] ) { $sum_1 += $check[$i] }
-    # second part, it's given that the array has an even number of elements
     my $j = $i + $length / 2;
+    if ( $check[$i] == $check[ $i + 1 ] ) { $sum_1 += $check[$i] }
+
     if ( $check[$i] == $check[$j] ) { $sum_2 += $check[$i] }
 }
 say "Part 1: $sum_1";
