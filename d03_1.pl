9272037fe73ffb92fe5ae40a3aca696a0cade4bc
Gustaf Erikson <gerikson@gmail.com>
2017-12-03 18:58:41 +0000

    Added solution files

---
+++ d03_1.pl
@@ -0,0 +1,50 @@
+# Advent of Code 2017 Day 3 - part 1
+# Problem link: http://adventofcode.com/2017/day/3
+#   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2017.html#d03
+#      License: http://gerikson.com/files/AoC2017/UNLICENSE
+###########################################################
+#!/usr/bin/perl
+use 5.016;    # implies strict, provides 'say'
+use warnings;
+use autodie;
+use List::Util qw/min/;
+
+#### INIT - load input data into array
+my @input;
+my $file = 'input.txt';
+open( my $fh, '<', "$file" );
+while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
+
+### CODE
+my $target = $input[0];
+
+sub diagonal_values {
+    # from Project Euler 28, the values of numbers on diagonals
+    my ($x) = @_;
+    my $n = 2 * $x + 1;
+    return [
+        $n * $n - 3 * $n + 3,
+        $n * $n - 2 * $n + 2,
+        $n * $n - $n + 1,
+        $n * $n
+    ];
+}
+
+my $x        = 0;
+my $distance = 0;
+while (1) {
+    my $diags = diagonal_values($x);
+
+    # find the "ring" where the target value lies
+    if ( $target >= $diags->[0] and $target <= $diags->[3] ) {
+
+        # the diagonal values represent the largest Manhattan distance 2x
+        # find the minimum distance to the diagonals
+        my $min = min( map { abs( $target - $diags->[$_] ) } ( 0, 1, 2, 3 ) );
+        $distance = 2 * $x - $min;
+        last;
+    }
+    $x++;
+}
+
+say $distance;
---
+++ d03_2.pl
@@ -0,0 +1,69 @@
+# Advent of Code 2017 Day 3 - part 2
+# Problem link: http://adventofcode.com/2017/day/3
+#   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2017.html#d03
+#      License: http://gerikson.com/files/AoC2017/UNLICENSE
+###########################################################
+#!/usr/bin/perl
+use 5.016;    # implies strict, provides 'say'
+use warnings;
+use autodie;
+use List::Util qw/sum/;
+use Data::Dumper;
+
+#### INIT - load input data into array
+my @input;
+my $file = 'input.txt';
+open( my $fh, '<', "$file" );
+while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
+
+### CODE
+
+my $M;
+my $dirs = [ [ 1, 0 ], [ 0, 1 ], [ -1, 0 ], [ 0, -1 ] ];
+sub adjacent_sum {
+    my ( $x, $y ) = @_;
+    my $sum = 0;
+    foreach my $i ( $x - 1, $x, $x + 1 ) {
+        foreach my $j ( $y - 1, $y, $y + 1 ) {
+            if ( defined $M->{$i}->{$j} ) {
+                $sum += $M->{$i}->{$j};
+            }
+        }
+    }
+    return $sum;
+}
+
+my $target = $input[0];
+
+my $current_val = 1;
+my ( $x, $y ) = ( 0, 0 );
+$M->{$x}->{$y} = $current_val;
+
+# should really use an iterator here but eff it...
+# just generate a bunch of steps (1,1,2,2,3,3,4,4,5,5...)
+# max value found by inspection of output during development
+my @steplengths;
+foreach my $step ( 1 .. 25 ) {
+    push @steplengths, $step;
+    push @steplengths, $step;
+}
+
+my $dir_idx = 0;
+LOOP: while (@steplengths) {
+
+    my $step = shift @steplengths;
+    if ( $dir_idx == 4 ) { $dir_idx = 0 }
+    while ( $step > 0 ) {
+        ( $x, $y ) =
+          ( $x + $dirs->[$dir_idx]->[0], $y + $dirs->[$dir_idx]->[1] );
+        $current_val = adjacent_sum( $x, $y );
+        if ( $current_val >= $target ) {
+            last LOOP;
+        }
+        $M->{$x}->{$y} = $current_val;
+        $step--;
+    }
+    $dir_idx++;
+
+}
+say $current_val;
