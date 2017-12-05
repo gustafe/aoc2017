e97fc8db4e514822c6fba291d18c676c61d64bbc
Gustaf Erikson <gerikson@gmail.com>
2017-12-05 11:02:53 +0000

    Solution added

---
+++ d05.pl
@@ -0,0 +1,49 @@
+# Advent of Code 2017 Day 5 - complete solution
+# Problem link: http://adventofcode.com/2017/day/5
+#   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2017.html#d05
+#      License: http://gerikson.com/files/AoC2017/UNLICENSE
+###########################################################
+#!/usr/bin/perl
+use 5.016;    # implies strict, provides 'say'
+use warnings;
+use autodie;
+
+#### INIT - load input data into array
+my $testing = 0;
+my @input;
+my $file = $testing ? 'test.txt' : 'input.txt';
+open( my $fh, '<', "$file" );
+while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
+
+### CODE
+
+sub debug {
+    say join ' ', @input;
+}
+# pass a non-false argument to enable part 2
+my $part2 = shift || 0;
+
+my $steps   = 0;
+my $pointer = 0;
+
+while ( $pointer >= 0 and $pointer < scalar @input ) {
+    my $jump = $input[$pointer];
+    if ( $jump == 0 ) {
+        $steps++;
+        $input[$pointer]++;
+        next;
+    }
+    else {
+        my $pos  = $pointer;
+        $pointer = $pointer + $jump;
+        $steps++;
+        if ( $jump >= 3 and $part2 ) {
+            $input[$pos]--;
+        }
+        else {
+            $input[$pos]++;
+        }
+    }
+    debug if $testing;
+}
+say $steps;
