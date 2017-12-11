98b34c5733a8c581bcc20d2cf7bb9dcb66be5b50
Gustaf Erikson <gerikson@gmail.com>
2017-12-11 08:14:30 +0000

    removed testing code path

--- d11.pl
+++ d11.pl
@@ -1,10 +1,13 @@
 #! /usr/bin/env perl
+# Advent of Code 2017 Day 11 - complete solution
+# Problem link: http://adventofcode.com/2017/day/11
+#   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2017.html#d11
+#      License: http://gerikson.com/files/AoC2017/UNLICENSE
+###########################################################
 use 5.016;
 use warnings;
 use autodie;
-# useful modules
-use List::Util qw/sum max/;
-use Data::Dumper;
+use List::Util qw/max/;
 
 #### INIT - load input data from file into array
 my $testing = 0;
@@ -14,7 +17,7 @@ open( my $fh, '<', "$file" );
 while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
 
 ### CODE
-# helpful page https://www.redblobgames.com/grids/hexagons/
+# x,y,z coordinates - see https://www.redblobgames.com/grids/hexagons/
 my %move = (
     n  => sub { [  0,  1, -1 ] },
     ne => sub { [  1,  0, -1 ] },
@@ -24,28 +27,29 @@ my %move = (
     nw => sub { [ -1,  1,  0 ] },
 );
 
-while (@input) {
-    my @dirs = split( /,/, shift @input );
-    my @path;
-    my $M = [ 0, 0, 0 ];
-    my $max_dist = 0;
-    while (@dirs) {
-        my $ins = shift @dirs;
-        push @path, $ins;
-        my $d = $move{$ins}->();
+my @dirs = split( /,/, shift @input );
+my @path;
 
-        for my $coord ( 0, 1, 2 ) {
-            $M->[$coord] = $M->[$coord] + $d->[$coord];
-        }
-        my $curr_dist = max( map { abs($_) } @$M );
-        if ( $curr_dist > $max_dist ) {
-            $max_dist = $curr_dist;
-        }
+#         x, y, z
+my $M = [ 0, 0, 0 ];
+my $max_dist = 0;
+while (@dirs) {
+    my $ins = shift @dirs;
+    push @path, $ins;
+    my $d = $move{$ins}->();
+
+    for my $coord ( 0, 1, 2 ) {
+        $M->[$coord] = $M->[$coord] + $d->[$coord];
     }
-    if ($testing) {
-        print join( ' -> ', @path ), ' ';
+    my $curr_dist = max( map { abs($_) } @$M );
+    if ( $curr_dist > $max_dist ) {
+        $max_dist = $curr_dist;
     }
-    say "End position: ", join( ',', @$M );
-    say "      Part 1: ", max( map { abs($_) } @$M );
-    say "      Part 2: ", $max_dist;
 }
+if ($testing) {
+    print join( ' -> ', @path ), ' ';
+}
+say "End position: ", join( ',', @$M );
+say "      Part 1: ", max( map { abs($_) } @$M );
+say "      Part 2: ", $max_dist;
+
