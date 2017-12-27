67f05f9ecaf302f29b87141b6be6de5ff90b1856
Gustaf Erikson <gerikson@gmail.com>
2017-12-27 08:51:54 +0100

    code cleanup

--- d25.pl
+++ d25.pl
@@ -8,13 +8,6 @@ use 5.016;
 use warnings;
 use autodie;
 
-# useful modules
-use List::Util qw/sum/;
-use Data::Dumper;
-
-#### INIT - load input data from file into array
-my $testing = 0;
-
 ### CODE
 my $tape;
 
